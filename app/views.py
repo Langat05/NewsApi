f576f7728344fa55016fac3ecdf81ae660380e2a
langat justus <justuslangat78@gmail.com>
2020-09-10 05:01:06 +0300

    added the articles headlines

--- app/views.py
+++ app/views.py
@@ -8,3 +8,19 @@ App = Flask(__name__)
 def index():
     newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
     topheadlines = newsapi.get_headlines(sources="abc-news")
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = [articles[i]]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+
