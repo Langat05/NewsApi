5dcdd54db0f3c388a896be430f57aca1167f74f5
langat justus <justuslangat78@gmail.com>
2020-09-10 04:54:50 +0300

    added news topheadlines sources

--- app/views.py
+++ app/views.py
@@ -7,3 +7,4 @@ App = Flask(__name__)
 @app.route('/')
 def index():
     newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_headlines(sources="abc-news")
