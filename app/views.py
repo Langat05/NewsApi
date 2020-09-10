bda3e3ebbd96d6bf509b4fced9c376652d2bd539
langat justus <justuslangat78@gmail.com>
2020-09-10 06:03:32 +0300

    created a new html file

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
-    topheadlines = newsapi.get_headlines(sources="abc-news")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
 
     articles = topheadlines['articles']
@@ -18,15 +18,15 @@ def index():
     img = []
 
     for i in range(len(articles)):
-        myarticles = [articles[i]]
+        myarticles = articles[i]
 
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
 
-    nowlist = zip(news, desc, img)    
+    mylist = zip(news, desc, img)    
 
-    return render_template('index.html', context = nowlist)
+    return render_template('index.html', context = mylist)
 
 if __name__ == "__main__":
     app.run(debug=True)
