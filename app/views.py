f81e3e753016630038eb09e37f0ed63234857984
langat justus <justuslangat78@gmail.com>
2020-09-10 06:07:50 +0300

    added router to the application

--- app/views.py
+++ app/views.py
@@ -28,6 +28,30 @@ def index():
 
     return render_template('index.html', context = mylist)
 
+@app.route('/bbc')
+def bbc():    
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)    
+
+    return render_template('bbc.html', context = mylist)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
