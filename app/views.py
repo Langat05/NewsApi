f3e2593202eaa501793e96ec8c9d3eac13f6cc4b
langat justus <justuslangat78@gmail.com>
2020-09-10 04:41:40 +0300

    added the news api key

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from newsapi import NewsApiClient
+
+
+App = Flask(__name__)
+
+@app.route('/')
+def index():
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
