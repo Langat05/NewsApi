fac6210d8af13976785f28e6facb5fe7f63c29a8
langat justus <justuslangat78@gmail.com>
2020-09-11 13:04:00 +0300

    added a new hmtl file

--- app/views.py
+++ app/views.py
@@ -60,6 +60,35 @@ def bbc():
 
     return render_template('bbc.html', context = mylist)
 
+@app.route('/fox')
+def fox():    
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_top_headlines(sources="fox-news")
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    url = []
+    img = []
+    
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+      
+
+    mylist = zip(news, desc, url, img)    
+
+    return render_template('fox.html', context = mylist)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
