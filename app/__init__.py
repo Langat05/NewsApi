bc81cb37f160a2c9b489113e8dfd05ca91cbaabc
langat justus <justuslangat78@gmail.com>
2020-09-11 16:05:27 +0300

    moved the config.py file to the root directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from app import error
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 
@@ -25,3 +27,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
