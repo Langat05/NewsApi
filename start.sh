a2844885406f601c48f3ef2a52fb201a74a719ae
langat justus <justuslangat78@gmail.com>
2020-09-11 23:49:54 +0300

    added a new html file and modified it

---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=d40c178ee0c6474f963ed683b651a3ab
+
+
+python3.8 views.py server
--- views.py
+++ views.py
@@ -5,19 +5,18 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
     topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
-
     articles = topheadlines['articles']
 
     desc = []
     news = []
     url = []
     img = []
-    
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -26,26 +25,23 @@ def index():
         desc.append(myarticles['description'])
         url.append(myarticles['url'])
         img.append(myarticles['urlToImage'])
-        
 
+    mylist = zip(news, desc, url, img)
 
-    mylist = zip(news, desc, url, img)    
+    return render_template('index.html', context=mylist)
 
-    return render_template('index.html', context = mylist)
 
 @app.route('/bbc')
-def bbc():    
+def bbc():
     newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
     topheadlines = newsapi.get_top_headlines(sources="bbc-news")
 
-
     articles = topheadlines['articles']
 
     desc = []
     news = []
     url = []
     img = []
-    
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -54,17 +50,16 @@ def bbc():
         desc.append(myarticles['description'])
         url.append(myarticles['url'])
         img.append(myarticles['urlToImage'])
-      
 
-    mylist = zip(news, desc, url, img)    
+    mylist = zip(news, desc, url, img)
 
-    return render_template('bbc.html', context = mylist)
+    return render_template('bbc.html', context=mylist)
 
-@app.route('/fox')
-def fox():    
+
+@app.route('/cnn')
+def cnn():
     newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
-    topheadlines = newsapi.get_top_headlines(sources="fox-news")
-
+    topheadlines = newsapi.get_top_headlines(sources="cnn")
 
     articles = topheadlines['articles']
 
@@ -72,7 +67,6 @@ def fox():
     news = []
     url = []
     img = []
-    
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -81,17 +75,37 @@ def fox():
         desc.append(myarticles['description'])
         url.append(myarticles['url'])
         img.append(myarticles['urlToImage'])
-      
 
-    mylist = zip(news, desc, url, img)    
+    mylist = zip(news, desc, url, img)
 
-    return render_template('fox.html', context = mylist)
+    return render_template('cnn.html', context=mylist)
 
 
+@app.route('/business')
+def business():
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_top_headlines(sources="business-insider")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    url = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, url, img)
+
+    return render_template('business.html', context=mylist)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
     app.run
-
-
