50061dd6e1cc29306fc6584ed770603083b6b7c5
langat justus <justuslangat78@gmail.com>
2020-09-11 16:17:25 +0300

    moved the test.py from app to test folder

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,15 @@ app.config.from_pyfile('config.py')
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
 
-from app import views
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+
+    return app
